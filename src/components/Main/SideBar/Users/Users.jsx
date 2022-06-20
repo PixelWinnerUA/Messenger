208f24a64e3c7962060f7a6aad8016544b2bbd3c
pixel <pixelgun3d2014@gmail.com>
2022-06-21 00:08:31 +0300

    20.06.2022

--- src/components/Main/SideBar/Users/Users.jsx
+++ src/components/Main/SideBar/Users/Users.jsx
@@ -6,48 +6,52 @@ import {CircularProgress} from "@mui/material";
 
 const Users = ({UsersList, SearchStatus}) => {
 
-    let Users = UsersList.map(item => <li key={item.userName}>
-        <div className="User">{
-            item.photo ?
-                <div style={{
-                    gridArea: "icon",
-                    background: `url(data:image/jpeg;base64,${item.photo.bytes})`, // img src ={`data:image/jpeg;base64,${item.photo.bytes}`}
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center center",
-                    height: 50,
-                    width: 50,
-                    overflow: "hidden",
-                    outline: "none",
-                    margin: "0 10px",
-                    borderRadius: "50%"
-                }}/>
-                :
-                <div style={{
-                    gridArea: "icon",
-                    background: "url(" + DefaultIcon + ")",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center center",
-                    height: 50,
-                    width: 50,
-                    overflow: "hidden",
-                    outline: "none",
-                    margin: "0 10px",
-                    borderRadius: "50%"
-                }}/>}
+    let Users = UsersList.length === 0 ?
+        <li style={{margin: "10px"}}>User is not found</li>
+        :
+        UsersList.map(item => <li key={item.userName}>
+            <div className="User">{
+                item.photo ?
+                    <div style={{
+                        gridArea: "icon",
+                        background: `url(data:image/jpeg;base64,${item.photo.bytes})`, // img src ={`data:image/jpeg;base64,${item.photo.bytes}`}
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center center",
+                        height: 50,
+                        width: 50,
+                        overflow: "hidden",
+                        outline: "none",
+                        margin: "0 10px",
+                        borderRadius: "50%"
+                    }}/>
+                    :
+                    <div style={{
+                        gridArea: "icon",
+                        background: "url(" + DefaultIcon + ")",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center center",
+                        height: 50,
+                        width: 50,
+                        overflow: "hidden",
+                        outline: "none",
+                        margin: "0 10px",
+                        borderRadius: "50%"
+                    }}/>}
 
-            <div className="info">
-                <div className="name">
-                    <p>{item.name}</p>
+                <div className="info">
+                    <div className="name">
+                        <p>{item.name}</p>
+                    </div>
+                    <div className="userName">
+                        <p>@{item.userName}</p>
+                    </div>
                 </div>
-                <div className="userName">
-                    <p>@{item.userName}</p>
-                </div>
+
             </div>
+        </li>)
 
-        </div>
-    </li>)
     return (
         <div className="Users">
             {SearchStatus ?
