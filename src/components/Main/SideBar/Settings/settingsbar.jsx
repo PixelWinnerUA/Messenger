d75fc7f9735776fd4796f5e8a65ea9207ec40300
pixel <pixelgun3d2014@gmail.com>
2022-07-04 18:11:02 +0300

    04.07.2022 using React-Query, refactoring.

--- src/components/Main/SideBar/Settings/settingsbar.jsx
+++ src/components/Main/SideBar/Settings/settingsbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "../../../../styles/SettingsBar.scss"
 
-const SettingsBar = ({setSearchInput, isActive, setActive}) => {
+const SettingsBar = ({fetchSearch, SearchInput, setSearchInput, isActive, setActive}) => {
+
+    useEffect(() => {
+        if (SearchInput) {
+            fetchSearch({input: SearchInput})
+        }
+    }, [SearchInput, fetchSearch])
 
     return (
         <div className="SettingsBar">
